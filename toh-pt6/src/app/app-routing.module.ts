51e0f67b52cd985d2de1b7bedb067769d739b3a7
lilongji <woshililongji@163.com>
2017-12-08 18:15:02 +0800

    add cf-college

--- toh-pt6/src/app/app-routing.module.ts
+++ toh-pt6/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 
 import { DashboardComponent }   from './dashboard.component';
 import { HeroesComponent }      from './heroes.component';
@@ -11,9 +11,11 @@ const routes: Routes = [
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes',     component: HeroesComponent }
 ];
-
+const config: ExtraOptions = { // 使用hash 避免刷新消失
+    useHash: true,
+};
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, config) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
